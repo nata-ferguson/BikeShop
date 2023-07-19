990f109c380d95716da933471c3a085877527c16
Natasha Ferguson <nata.ferguson@outlook.com>
2023-07-19 14:37:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bikeshop/UI/ProductList.java
@@ -0,0 +1,16 @@
+package com.example.bikeshop.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bikeshop.R;
+
+public class ProductList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+    }
+}
