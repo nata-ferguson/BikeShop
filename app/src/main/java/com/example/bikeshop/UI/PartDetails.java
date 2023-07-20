dd555064e53d26d3922cd9e10330243bab50f55e
Natasha Ferguson <nata.ferguson@outlook.com>
2023-07-20 10:36:51 -0500

    added activities for products and parts screens

---
+++ app/src/main/java/com/example/bikeshop/UI/PartDetails.java
@@ -0,0 +1,18 @@
+package com.example.bikeshop.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bikeshop.R;
+
+public class PartDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part_details);
+
+
+    }
+}
